963c5140df164b001e0ffece345ad6d46127d0de
Nic Schwab <93524780+Nics-Github@users.noreply.github.com>
2025-09-18 09:35:29 +0000

    checking tests on github classroom

--- hw01.py
+++ hw01.py
@@ -22,26 +22,30 @@
 # # Part 1: Basic Operations
 # # =============================================
 # # Your code for part 1 under this line and before the print statements
-
-# result1 = ((3*x) - (9*y) ) / ( (2*a)*(b-c) )
-# print("Part 1: x =",x)
-# print("Part 1: y =",y)
-# print("Part 1: a =",a)
-# print("Part 1: b =",b)
-# print("Part 1: c =",c)
-# print("Part 1: result =", result1)
+x = 27
+y = 1
+a = 1.5
+b = 7
+c = -1
+result1 = ((3*x) - (9*y) ) / ( (2*a)*(b-c) )
+print("Part 1: x =",x)
+print("Part 1: y =",y)
+print("Part 1: a =",a)
+print("Part 1: b =",b)
+print("Part 1: c =",c)
+print("Part 1: result =", result1)
 # # End of Part 1 ----------------------
-# result = (3*x-9*y)/(2*a*(b-c))
+
 
 # # Part 2: Power
 # # =============================================
 # # Your code for part 2 under this line and before the print statements
-# x = 5
-# y = -3
-# result2 = x**2*y**4
-# print("Part 2: x =",x)
-# print("Part 2: y =",y)
-# print("Part 2: result =", result2)
+x = 5
+y = -3
+result2 = x**2*y**4
+print("Part 2: x =",x)
+print("Part 2: y =",y)
+print("Part 2: result =", result2)
 # # End of Part 2 ----------------------
 
 
@@ -49,18 +53,19 @@
 # # Part 3: Integer divide
 # # =============================================
 # # Your code for part 3 under this line and before the print statements
-
-# print("Part 3: a =",a)
-# print("Part 3: b =",b)
-# print("Part 3: result =", result3)
+a = 100
+b = 13
+result3 = a//b
+print("Part 3: a =",a)
+print("Part 3: b =",b)
+print("Part 3: result =", result3)
 # End of Part 3 ----------------------
 
 
 # Part 4: Modulo
 # =============================================
 # Your code for part 4 under this line and before the print statements
-treats = 100
-dogs = 13
-result4 = treats%dogs
+
+result4 = a%b
 print("Part 4: result =", result4)
 # End of Part 4 ----------------------
